a7d5dbdc6c68abe9e570460cc9727eb475a75b28
tr-osfh <nstim2006@gmail.com>
2025-03-18 11:31:03 +0300

    123456

---
+++ src/main/java/root/files/commands/ExecutedCommands.java
@@ -0,0 +1,24 @@
+package root.files.commands;
+
+import java.util.ArrayList;
+
+public class ExecutedCommands{
+
+    public ExecutedCommands(ArrayList<String> executedCommands) {
+        this.executedCommands = executedCommands;
+    }
+
+    private ArrayList<String> executedCommands = new ArrayList<String>();
+
+    public void addExecutedCommand(String line){
+        executedCommands.add(line);
+    }
+
+    public void setExecutedCommands(ArrayList<String> executedCommands) {
+        this.executedCommands = executedCommands;
+    }
+
+    public ArrayList<String> getExecutedCommands() {
+        return executedCommands;
+    }
+}
